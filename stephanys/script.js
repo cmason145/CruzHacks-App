d081331655845bb0b1c513faba395f1db10a875e
Stephany K <stephanykleen@gmail.com>
2023-02-04 13:51:52 -0800

    features udpated
    based on figma template designed

---
+++ stephanys/script.js
@@ -0,0 +1,8 @@
+let firstName = 'Matt';
+let imageUrl = 'images/profile1.png';
+let description = 'Matt is 6ft,';
+
+
+document.querySelector(".image").innerHTML=`${firstName}`;
+document.querySelector(".name").innerHTML=`${imageUrl}`;
+document.querySelector(".description").innerHTML=`${description}`;
