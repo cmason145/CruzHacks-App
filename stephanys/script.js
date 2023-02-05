381e0c29a0362200b0bc9bcad22366ed5ae8ff9f
Stephany K <stephanykleen@gmail.com>
2023-02-04 21:26:54 -0800

    added javascript to create responsive buttons

--- stephanys/script.js
+++ stephanys/script.js
@@ -8,3 +8,23 @@ function initMap() {
     mapId: '28128416ff9651c8',
   });
 }
+
+//-------------Index File, Call The Police Button-----------//
+
+function callPoliceFunction() {
+  let doubleCheck = prompt("Do you want to contact the Campus Police? Yes/No");
+  doubleCheck = doubleCheck.toLowerCase();
+  doubleCheck = doubleCheck.trim(" ");
+  if( doubleCheck === "yes"){
+alert( "This is their number (831)-459-2231")
+  } else{
+    alert("Be safe. Goodbye 👋🏽");
+  }
+}
+
+let callNow = document.querySelector("#call-campus-police");
+callNow.addEventListener('click',callPoliceFunction);
+
+
+
+
